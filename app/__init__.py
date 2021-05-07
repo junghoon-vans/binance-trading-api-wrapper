247dae060e05eba40fe94c612a4f0a75d6c48c3a
Jeonghun-Ban <junghoon.ban@gmail.com>
2021-05-07 10:34:13 +0000

    feat: Create launcher and server_builder

---
+++ app/__init__.py
@@ -0,0 +1,4 @@
+from flask import Flask
+
+def server_builder():
+    return Flask(__name__)
---
+++ launcher.py
@@ -0,0 +1,6 @@
+import app
+
+server = app.server_builder()
+
+if __name__ == '__main__':
+    server.run()
