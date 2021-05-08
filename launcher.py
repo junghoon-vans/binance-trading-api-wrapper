1ae61272b8e4d01fa3c795695184881b212b8d0d
Jeonghun-Ban <junghoon.ban@gmail.com>
2021-05-08 05:30:00 +0000

    feat: Add argpaser to launcher to provide command line options

--- launcher.py
+++ launcher.py
@@ -1,7 +1,16 @@
+import argparse
+
 from app import server_builder
 
 
 server = server_builder()
 
-if __name__ == '__main__':
-    server.run()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="binance trading bot launcher")
+    parser.add_argument(
+        "-a", "--address", type=str, default="0.0.0.0", help="ip address"
+    )
+    parser.add_argument("-p", "--port", type=int, default=5000, help="port number")
+    args = parser.parse_args()
+
+    server.run(args.address, args.port)
