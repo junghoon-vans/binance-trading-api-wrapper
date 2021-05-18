b9f295a80e2e55fa350804eba322432ce9631f3b
Jeonghun-Ban <junghoon.ban@gmail.com>
2021-05-18 11:55:03 +0000

    feat: Add change_margin_type, change_leverage, post_order funcs

--- api/route/__init__.py
+++ api/route/__init__.py
@@ -1,8 +1,9 @@
 from flask_restful import Api
 
-from api.route.trade import Buy, Sell
+from api.route.trade import ChangeMarginType, ChangeLeverage, PostOrder
 
 
 def binding_route(api: Api):
-    api.add_resource(Buy, "/trade/buy")
-    api.add_resource(Sell, "/trade/sell")
+    api.add_resource(ChangeMarginType, "/trade/change_margin_type")
+    api.add_resource(ChangeLeverage, "/trade/change_leverage")
+    api.add_resource(PostOrder, "/trade/post_order")
--- api/route/trade.py
+++ api/route/trade.py
@@ -1,11 +1,55 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+
+from binance_f.base.printobject import PrintBasic, PrintMix
+
+from api import get_server
 
 
-class Buy(Resource):
+class ChangeMarginType(Resource):
     def get(self):
-        return {"trade": "buy"}
+        server = get_server()
+
+        parser = reqparse.RequestParser()
+        parser.add_argument("symbol", type=str)
+        parser.add_argument("marginType", type=str)
+        args = parser.parse_args()
+
+        result = server.request.change_margin_type(args.symbol, args.marginType)
+        PrintBasic.print_obj(result)
 
 
-class Sell(Resource):
+class ChangeLeverage(Resource):
     def get(self):
-        return {"trade": "sell"}
+        server = get_server()
+
+        parser = reqparse.RequestParser()
+        parser.add_argument("symbol", type=str)
+        parser.add_argument("leverage", type=int)
+        args = parser.parse_args()
+
+        result = server.request.change_initial_leverage(args.symbol, args.leverage)
+        PrintBasic.print_obj(result)
+
+
+class PostOrder(Resource):
+    def get(self):
+        server = get_server()
+
+        parser = reqparse.RequestParser()
+        parser.add_argument("symbol", type=str)
+        parser.add_argument("side", type=str)
+        parser.add_argument("ordertype", type=str)
+        parser.add_argument("stopPrice", type=int)
+        parser.add_argument("closePosition", type=bool)
+        parser.add_argument("positionSide", type=str)
+        args = parser.parse_args()
+
+        result = server.request.post_order(
+            args.symbol,
+            args.side,
+            args.ordertype,
+            args.stopPrice,
+            args.closePosition,
+            args.positionSide,
+        )
+        PrintMix.print_data(result)
