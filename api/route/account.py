1230bf549dcec90497001ffec3e19b408656e32e
Jeonghun-Ban <junghoon.ban@gmail.com>
2021-05-26 14:59:01 +0000

    refactor: Update account route to change url mapping and fix type_error

--- api/route/account.py
+++ api/route/account.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 
 from api import get_server
 
@@ -6,57 +6,67 @@ from api import get_server
 blueprint = Blueprint("account", __name__, url_prefix="/account")
 
 
-@blueprint.route("/account_transfer/")
-def account_transfer():
-    server = get_server()
-    params = request.args.to_dict()
-    return server.request.futures_account_transfer(**params)
-
-
-@blueprint.route("/transfer_history/")
-def transfer_history():
-    server = get_server()
-    params = request.args.to_dict()
-    return server.request.transfer_history(**params)
-
-
-@blueprint.route("/account_balance/")
-def account_balance():
-    server = get_server()
-    params = request.args.to_dict()
-    return server.request.futures_account_balance(**params)
-
-
-@blueprint.route("/account/")
+@blueprint.route("/")
 def account():
     server = get_server()
-    params = request.args.to_dict()
-    return server.request.futures_account(**params)
+    return jsonify(
+        server.request.futures_account()
+    )
 
 
-@blueprint.route("/position_information/")
-def position_information():
+@blueprint.route("/balance/")
+def account_balance():
     server = get_server()
+    return jsonify(
+        server.request.futures_account_balance()
+    )
+
+
+@blueprint.route("/transfer/", methods=('GET', 'POST'))
+def transfer():
     params = request.args.to_dict()
-    return server.request.futures_position_information(**params)
+    server = get_server()
+    if request.method == 'POST':
+        return jsonify(
+            server.request.futures_account_transfer(**params)
+        )
+    elif request.method == 'GET':
+        return jsonify(
+            server.request.transfer_history(**params)
+        )
 
 
-@blueprint.route("/account_trades/")
+@blueprint.route("/trades/")
 def account_trades():
+    symbol = request.args.get('symbol')
     server = get_server()
-    params = request.args.to_dict()
-    return server.request.futures_account_trades(**params)
+    return jsonify(
+        server.request.futures_account_trades(symbol=symbol)
+    )
 
 
-@blueprint.route("/income_history/")
+@blueprint.route("/position/")
+def position_information():
+    symbol = request.args.get('symbol')
+    server = get_server()
+    return jsonify(
+        server.request.futures_position_information(symbol=symbol)
+    )
+
+
+@blueprint.route("/income/")
 def income_history():
-    server = get_server()
     params = request.args.to_dict()
-    return server.request.futures_income_history(**params)
+    server = get_server()
+    return jsonify(
+        server.request.futures_income_history(**params)
+    )
 
 
-@blueprint.route("/leverage_bracket/")
+@blueprint.route("/leverage-bracket/")
 def leverage_bracket():
+    symbol = request.args.get('symbol')
     server = get_server()
-    params = request.args.to_dict()
-    return server.request.futures_leverage_bracket(**params)
+    return jsonify(
+        server.request.futures_leverage_bracket(symbol=symbol)
+    )
