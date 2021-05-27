baf4fc113409c7d25bda5185c1049ebc479a19da
Jeonghun-Ban <junghoon.ban@gmail.com>
2021-05-27 14:54:48 +0000

    refactor: Update trade route to make api restful

--- api/route/trade.py
+++ api/route/trade.py
@@ -6,105 +6,86 @@ from api import get_server
 blueprint = Blueprint("trade", __name__, url_prefix="/trade")
 
 
-@blueprint.route("/create_order/")
-def create_order():
-    server = get_server()
+@blueprint.route("/order", methods=["GET", "POST", "DELETE"])
+def order():
     params = request.args.to_dict()
-    server.request.futures_create_order(**params)
-
-
-@blueprint.route("/place_batch_order/")
-def place_batch_order():
     server = get_server()
+    if request.method == "POST":
+        server.request.futures_create_order(**params)
+    elif request.method == "GET":
+        server.request.futures_get_order(**params)
+    elif request.method == "DELETE":
+        server.request.futures_cancel_order(**params)
+
+
+@blueprint.route("/order/multiple", methods=["POST", "DELETE"])
+def mutiple_orders():
     params = request.args.to_dict()
-    server.request.futures_place_batch_order(**params)
-
-
-@blueprint.route("/get_order/")
-def get_order():
     server = get_server()
-    params = request.args.to_dict()
-    server.request.futures_get_order(**params)
+    if request.method == "POST":
+        server.request.futures_place_batch_order(**params)
+    elif request.method == "DELETE":
+        server.request.futures_cancel_orders(**params)
 
 
-@blueprint.route("/get_open_orders/")
+@blueprint.route("/order/open")
 def get_open_orders():
-    server = get_server()
     params = request.args.to_dict()
+    server = get_server()
     server.request.futures_get_open_orders(**params)
 
 
-@blueprint.route("/get_all_orders/")
-def get_all_orders():
-    server = get_server()
+@blueprint.route("/order/all", methods=["GET", "DELETE"])
+def all_orders():
     params = request.args.to_dict()
-    server.request.futures_get_all_orders(**params)
-
-
-@blueprint.route("/cancel_order/")
-def cancel_order():
     server = get_server()
-    params = request.args.to_dict()
-    server.request.futures_cancel_order(**params)
+    if request.method == "GET":
+        server.request.futures_get_all_orders(**params)
+    elif request.method == "DELETE":
+        server.request.futures_cancel_all_open_orders(**params)
 
 
-@blueprint.route("/cancel_all_open_orders/")
-def cancel_all_open_orders():
-    server = get_server()
-    params = request.args.to_dict()
-    server.request.futures_cancel_all_open_orders(**params)
-
-
-@blueprint.route("/cancel_orders/")
-def cancel_orders():
-    server = get_server()
-    params = request.args.to_dict()
-    server.request.futures_cancel_orders(**params)
-
-
-@blueprint.route("/change_leverage/")
+@blueprint.route("/leverage", methods=["POST"])
 def change_leverage():
-    server = get_server()
-    params = request.args.to_dict()
-    return server.request.futures_change_leverage(**params)
+    if request.method == "POST":
+        params = request.args.to_dict()
+        server = get_server()
+        return server.request.futures_change_leverage(**params)
 
 
-@blueprint.route("/change_margin_type/")
+@blueprint.route("/margin-type", methods=["POST"])
 def change_margin_type():
-    server = get_server()
+    if request.method == "POST":
+        params = request.args.to_dict()
+        server = get_server()
+        return server.request.futures_change_margin_type(**params)
+
+
+@blueprint.route("/position-margin", methods=["GET", "POST"])
+def position_margin():
     params = request.args.to_dict()
-    return server.request.futures_change_margin_type(**params)
-
-
-@blueprint.route("/change_position_margin/")
-def change_position_margin():
     server = get_server()
+    if request.method == "GET":
+        return server.request.futures_position_margin_history(**params)
+    elif request.method == "POST":
+        return server.request.futures_change_position_margin(**params)
+
+
+@blueprint.route("/mode/position", methods=["GET", "POST"])
+def position_mode():
     params = request.args.to_dict()
-    return server.request.futures_change_position_margin(**params)
-
-
-@blueprint.route("/change_position_mode/")
-def change_position_mode():
     server = get_server()
-    params = request.args.to_dict()
-    return server.request.futures_change_position_mode(**params)
+    if request.method == "GET":
+        return server.request.futures_get_position_mode(**params)
+    elif request.method == "POST":
+        return server.request.futures_change_position_mode(**params)
 
 
-@blueprint.route("/get_position_mode/")
-def get_position_mode():
-    server = get_server()
-    params = request.args.to_dict()
-    return server.request.futures_get_position_mode(**params)
-
-
-@blueprint.route("/change_multi_assets_mode/")
+@blueprint.route("mode/multi-asset", methods=["GET", "POST"])
 def change_multi_assets_mode():
-    server = get_server()
     params = request.args.to_dict()
-    return server.request.futures_change_multi_assets_mode(**params)
-
-
-@blueprint.route("/get_multi_assets_mode/")
-def get_multi_assets_mode():
     server = get_server()
-    return server.request.futures_get_multi_assets_mode()
+    if request.method == "GET":
+        return server.request.futures_get_multi_assets_mode()
+    elif request.method == "POST":
+        return server.request.futures_change_multi_assets_mode(**params)
