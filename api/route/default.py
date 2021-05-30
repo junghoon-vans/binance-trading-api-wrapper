461fb96b9a28c164b587ccf80216a7f73bbc861b
Jeonghun-Ban <junghoon.ban@gmail.com>
2021-05-30 04:04:20 +0000

    refactor: Add json response to stream function

--- api/route/default.py
+++ api/route/default.py
@@ -6,39 +6,40 @@ from api import get_server
 blueprint = Blueprint("default", __name__, url_prefix="/")
 
 
-@blueprint.route("/ping/")
+@blueprint.route("/ping")
 def ping():
     server = get_server()
     result = server.request.futures_ping()
     return jsonify(result)
 
 
-@blueprint.route("/time/")
+@blueprint.route("/time")
 def time():
     server = get_server()
     result = server.request.futures_time()
     return jsonify(result)
 
 
-@blueprint.route("/exchange-info/")
+@blueprint.route("/exchange-info")
 def exchange_info():
     server = get_server()
     result = server.request.futures_exchange_info()
     return jsonify(result)
 
 
-@blueprint.route("/stream/", methods=("GET", "PUT", "DELETE"))
+@blueprint.route("/stream", methods=("GET", "PUT", "DELETE"))
 def stream():
     server = get_server()
     result = []
 
     if request.method == "GET":
-        result = server.request.futures_stream_get_listen_key()
+        listen_key = server.request.futures_stream_get_listen_key()
+        result = {"listenKey": listen_key}
     elif request.method == "PUT":
-        listenKey = request.args.get("listenKey")
-        result = server.request.futures_stream_keepalive(listenKey=listenKey)
+        listen_key = request.args.get("listenKey")
+        result = server.request.futures_stream_keepalive(listenKey=listen_key)
     elif request.method == "DELETE":
-        listenKey = request.args.get("listenKey")
-        result = server.request.futures_stream_close(listenKey=listenKey)
+        listen_key = request.args.get("listenKey")
+        result = server.request.futures_stream_close(listenKey=listen_key)
 
     return jsonify(result)
