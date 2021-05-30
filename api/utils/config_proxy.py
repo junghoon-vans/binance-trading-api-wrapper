1919bec7da15fe6ea21b00958d31c895d5a1f9dc
Jeonghun-Ban <junghoon.ban@gmail.com>
2021-05-30 01:32:07 +0000

    refactor: Update config_proxy to change func name

--- api/utils/config_proxy.py
+++ api/utils/config_proxy.py
@@ -2,10 +2,10 @@ import yaml
 
 
 def load_config(env: str):
-    config_file = get_config_file(env)
-    with open(config_file) as f:
+    config_file_path = get_config_file_path(env)
+    with open(config_file_path) as f:
         return yaml.load(f, Loader=yaml.FullLoader)
 
 
-def get_config_file(env: str):
+def get_config_file_path(env: str):
     return "configs/" + env + ".yaml"
