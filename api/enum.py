f0e7540521eca419e2d04f48e253f4e57437ca57
Jeonghun-Ban <junghoon.ban@gmail.com>
2021-06-03 14:21:02 +0000

    feat: Create trade schema

--- api/enum.py
+++ api/enum.py
@@ -52,3 +52,41 @@ class IncomeType(Enum):
     FUNDING_FEE = "FUNDING_FEE"
     COMMISSION = "COMMISSION"
     INSURANCE_CLEAR = "INSURANCE_CLEAR"
+
+
+class OrderSide(Enum):
+    BUY = "BUY"
+    SELL = "SELL"
+
+
+class PositionSide(Enum):
+    BOTH = "BOTH"
+    LONG = "LONG"
+    SHORT = "SHORT"
+
+
+class TimeInForce(Enum):
+    GTC = "GTC"
+    IOC = "IOC"
+    FOK = "FOK"
+    GTX = "GTX"
+
+
+class OrderType(Enum):
+    LIMIT = "LIMIT"
+    MARKET = "MARKET"
+    STOP = "STOP"
+    STOP_MARKET = "STOP_MARKET"
+    TAKE_PROFIT = "TAKE_PROFIT"
+    TAKE_PROFIT_MARKET = "TAKE_PROFIT_MARKET"
+    TRAILING_STOP_MARKET = "TRAILING_STOP_MARKET"
+
+
+class OrderRespType(Enum):
+    ACK = "ACK"
+    RESULT = "RESULT"
+
+
+class FuturesMarginType(Enum):
+    ISOLATED = "ISOLATED"
+    CROSSED = "CROSSED"
--- api/schema/__init__.py
+++ api/schema/__init__.py
@@ -28,6 +28,25 @@ from api.schema.market import (
     OpenInterestStatisticsSchema,
 )
 
+from api.schema.trade import (
+    GetOrderSchema,
+    PostOrderSchema,
+    DeleteOrderSchema,
+    PostMultipleOrderSchema,
+    DeleteMultipleOrderSchema,
+    GetOpenOrderSchema,
+    GetAllOrderSchema,
+    DeleteAllOrderSchema,
+    ChangeLeverageSchema,
+    ChangeMarginTypeSchema,
+    GetPositionMarginSchema,
+    PostPositionMarginSchema,
+    GetPositionModeSchema,
+    PostPositionModeSchema,
+    GetMultiAssetModeSchema,
+    PostMultiAssetModeSchema,
+)
+
 
 ping_schema = EmptyDictSchema()
 time_schema = TimeSchema()
@@ -54,3 +73,20 @@ market_symbol_price_ticker_schema = SymbolPriceTickerSchema()
 market_symbol_orderbook_ticker_schema = SymbolOrderbookTickerSchema()
 market_open_interest_schema = OpenInterestSchema()
 market_open_interest_statistics_schema = OpenInterestStatisticsSchema()
+
+trade_get_order_schema = GetOrderSchema()
+trade_post_order_schema = PostOrderSchema()
+trade_delete_order_schema = DeleteOrderSchema()
+trade_post_multiple_order_schema = PostMultipleOrderSchema()
+trade_delete_multiple_order_schema = DeleteMultipleOrderSchema()
+trade_get_open_order_schema = GetOpenOrderSchema()
+trade_get_all_order_schema = GetAllOrderSchema()
+trade_delete_all_order_schema = DeleteAllOrderSchema()
+trade_change_leverage_schema = ChangeLeverageSchema()
+trade_change_margin_type_schema = ChangeMarginTypeSchema()
+trade_get_position_margin_schema = GetPositionMarginSchema()
+trade_post_position_margin_schema = PostPositionMarginSchema()
+trade_get_position_mode_schema = GetPositionModeSchema()
+trade_post_position_mode_schema = PostPositionModeSchema()
+trade_get_multi_asset_mode_schema = GetMultiAssetModeSchema()
+trade_post_multi_asset_mode_schema = PostMultiAssetModeSchema()
