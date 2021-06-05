cbfc9d9df4145ea30830006764ddcf708b3689f6
Jeonghun-Ban <junghoon.ban@gmail.com>
2021-06-05 09:50:01 +0000

    docs: Create apidocs of default using doc string

--- api/route/default.py
+++ api/route/default.py
@@ -10,6 +10,17 @@ blueprint = DocumentedBlueprint("default", __name__, url_prefix="/")
 
 @blueprint.route("/ping", methods=["GET"])
 def ping() -> Response:
+    """
+    Test Connectivity
+    ---
+    get:
+      description: Test connectivity to the Rest API.
+      responses:
+        200:
+          content:
+            application/json: {}
+          description: OK
+    """
     server = get_server()
     response = jsonify(server.request.futures_ping())
     return response
@@ -17,6 +28,18 @@ def ping() -> Response:
 
 @blueprint.route("/time", methods=["GET"])
 def time() -> Response:
+    """
+    Check Server Time
+    ---
+    get:
+      description: Test connectivity to the Rest API and get the current server time.
+      responses:
+        200:
+          content:
+            application/json:
+              schema: TimeSchema
+          description: OK
+    """
     server = get_server()
     response = jsonify(time_schema.dump(server.request.futures_time()))
     return response
@@ -24,6 +47,18 @@ def time() -> Response:
 
 @blueprint.route("/stream", methods=["GET"])
 def stream() -> Response:
+    """
+    User Data Stream
+    ---
+    get:
+      description: Generate a Listen Key
+      responses:
+        200:
+          content:
+            application/json:
+              schema: StreamSchema
+          description: OK
+    """
     server = get_server()
     listen_key = server.request.futures_stream_get_listen_key()
     response = jsonify(stream_schema.dump({"listenKey": listen_key}))
@@ -32,6 +67,30 @@ def stream() -> Response:
 
 @blueprint.route("/stream/<listen_key>", methods=["PUT", "DELETE"])
 def modify_stream(listen_key) -> Response:
+    """
+    User Data Stream
+    ---
+    put:
+      description: Ping/Keep-alive a Listen Key
+      parameters:
+        - in: path
+          name: listenKey
+      responses:
+        200:
+          content:
+            application/json: {}
+          description: OK
+    delete:
+      description: Close a ListenKey
+      parameters:
+        - in: path
+          name: listenKey
+      responses:
+        200:
+          content:
+            application/json: {}
+          description: OK
+    """
     server = get_server()
     if request.method == "PUT":
         response = jsonify(
