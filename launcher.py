36b27f7fe39eb5557e2db8f51a35abdec9850de2
Jeonghun-Ban <junghoon.ban@gmail.com>
2021-06-06 03:13:25 +0000

    refactor: Restructure code to increase readability.

--- launcher.py
+++ launcher.py
@@ -4,27 +4,33 @@ from api import get_server
 from api.route import init_route
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="binance trading api launcher")
-    parser.add_argument(
-        "-a",
-        "--address",
-        type=str,
-        default="0.0.0.0",
-        help="ip address binding HTTP server",
-    )
-    parser.add_argument(
-        "-p", "--port", type=int, default=5000, help="port number binding HTTP server"
-    )
-    parser.add_argument(
-        "-e",
-        "--environment",
-        type=str,
-        default="production",
-        help="environment to running api server",
-    )
+parser = argparse.ArgumentParser(description="binance trading api launcher")
+parser.add_argument(
+    "-a",
+    "--address",
+    type=str,
+    default="0.0.0.0",
+    help="ip address binding HTTP server",
+)
+parser.add_argument(
+    "-p", "--port", type=int, default=5000, help="port number binding HTTP server"
+)
+parser.add_argument(
+    "-e",
+    "--environment",
+    type=str,
+    default="production",
+    help="environment to running api server",
+)
+
+
+def main():
     args = parser.parse_args()
 
     server = get_server(env=args.environment)
     init_route(app=server.app)
     server.app.run(host=args.address, port=args.port)
+
+
+if __name__ == "__main__":
+    main()
